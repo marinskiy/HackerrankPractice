9a15c2bdcc168d6a7e13222f74af827c72a8c7cb
Alexander Marinskiy <marinskiy@gmail.com>
2018-04-20 13:25:37 +0300

    add Data Structures/Arrays/Arrays - DS.py

---
+++ Data Structures/Arrays/Arrays - DS.py
@@ -0,0 +1,9 @@
+def reverseArray(arr):
+    result = arr[::-1]
+    return result
+
+
+arrСount = int(input())
+arr = list(map(int, input().rstrip().split()))
+result = reverseArray(arr)
+print(*result)
