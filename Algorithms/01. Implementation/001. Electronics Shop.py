ff490dbf032999981e6765ac6f88cd726d63656f
Alexander Marinskiy <marinskiy@gmail.com>
2018-04-20 14:08:01 +0300

    add Algorithms\01. Implementation\001. Electronics Shop.py

---
+++ Algorithms/01. Implementation/001. Electronics Shop.py
@@ -0,0 +1,14 @@
+def getMoneySpent(keyboards, drives, b):
+    result = -1
+    for i in range(1, len(keyboards)):
+        for j in range(1, len(drives)):
+            if result < keyboards[i] + drives[j] <= b:
+                result = keyboards[i] + drives[j]
+    return result
+
+
+b, n, m = map(int, input().split())
+keyboards = list(map(int, input().rstrip().split()))
+drives = list(map(int, input().rstrip().split()))
+moneySpent = getMoneySpent(keyboards, drives, b)
+print(moneySpent)
