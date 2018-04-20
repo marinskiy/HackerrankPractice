b363d00f1d7a4fae93e407e27c4ef2457c86533b
Alexander Marinskiy <marinskiy@gmail.com>
2018-04-20 13:13:00 +0300

    add 001. Designer Door Mat.py

---
+++ Python/Strings/001. Designer Door Mat.py
@@ -0,0 +1,9 @@
+height, length = map(int, input().split())
+for i in range(0, height // 2):
+    s = '.|.' * (i * 2 + 1)
+    print(s.center(length,'-'))
+print('WELCOME'.center(length, '-'))
+for i in range(height // 2 - 1, -1, -1):
+    s = '.|.' * (i * 2 + 1)
+    print(s.center(length,'-'))
+    
