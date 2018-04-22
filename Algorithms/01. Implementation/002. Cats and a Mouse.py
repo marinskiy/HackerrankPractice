df8eff68200c8050c676c068817aaec35cddf64f
Alexander Marinskiy <marinskiy@gmail.com>
2018-04-22 16:04:28 +0300

    add 002. Cats and a Mouse

---
+++ Algorithms/01. Implementation/002. Cats and a Mouse.py
@@ -0,0 +1,15 @@
+def catAndMouse(catA, catB, mouseC):
+    distanceA = abs(catA - mouseC)
+    distanceB = abs(catB - mouseC)
+    if distanceA > distanceB:
+        print('Cat B')
+    elif distanceA == distanceB:
+        print('Mouse C')
+    else:
+        print('Cat A')
+
+
+n = int(input())
+for i in range(n):
+    catA, catB, mouseC = map(int, input().split())
+    catAndMouse(catA, catB, mouseC)
