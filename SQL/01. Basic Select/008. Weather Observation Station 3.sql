220499f8a1170575aa57ecb6cb25778e048ca98f
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-07 10:29:17 +0300

    link update

--- SQL/01. Basic Select/008. Weather Observation Station 3.sql
+++ SQL/01. Basic Select/008. Weather Observation Station 3.sql
@@ -1,4 +1,4 @@
--- This is the solution to https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+-- This is the solution to https://www.hackerrank.com/challenges/weather-observation-station-3/problem
 
 
 SELECT DISTINCT CITY
