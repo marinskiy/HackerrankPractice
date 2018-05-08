59a1cdd7df8ab25530816a187473bb6796c50c82
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-08 16:43:56 +0300

    add 010. Weather Observation Station 5

---
+++ SQL/01. Basic Select/010. Weather Observation Station 5.sql
@@ -0,0 +1,16 @@
+-- This is the solution to https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY), CITY)
+WHERE ROWNUM = 1;
+
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY) DESC, CITY)
+WHERE ROWNUM = 1;
