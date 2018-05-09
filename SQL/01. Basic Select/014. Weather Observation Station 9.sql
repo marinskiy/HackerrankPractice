732277e811dc6e04d0a3b094b9cbfe8b5e861000
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-09 17:56:53 +0300

    add 014. Weather Observation Station 9

---
+++ SQL/01. Basic Select/014. Weather Observation Station 9.sql
@@ -0,0 +1,6 @@
+-- This is the solution to https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE SUBSTR (CITY, 1, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
