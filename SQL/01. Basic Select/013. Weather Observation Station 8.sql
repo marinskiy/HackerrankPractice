26ba40fdd62251fc278e0c58d764ad64e0251dee
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-09 14:54:45 +0300

    add 013. Weather Observation Station 8

---
+++ SQL/01. Basic Select/013. Weather Observation Station 8.sql
@@ -0,0 +1,8 @@
+-- This is the solution to https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE
+    SUBSTR (CITY, LENGTH(CITY), 1) IN ('a', 'e', 'i', 'o', 'u')
+    AND SUBSTR (CITY, 1, 1) IN ('A', 'E', 'I', 'O', 'U');
