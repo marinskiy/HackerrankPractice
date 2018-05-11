9932cbc3d26d2a91faafd79a72bd1f53dbf60484
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-11 11:53:09 +0300

    add 004. The Report

---
+++ SQL/04. Basic Join/004. The Report.sql
@@ -0,0 +1,10 @@
+-- This is the solution to https://www.hackerrank.com/challenges/the-report/problem
+
+
+SELECT
+    CASE WHEN G.Grade > 7 THEN S.Name ELSE 'NULL' END AS NameOrNull
+    , G.Grade
+    , S.Marks
+FROM Students S
+JOIN Grades G ON S.Marks BETWEEN G.Min_Mark AND G.Max_Mark
+ORDER BY G.Grade DESC, NameOrNull ASC, S.Marks ASC;
