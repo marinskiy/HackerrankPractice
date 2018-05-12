445f49102cdb3a48b1d402019a30040c2b8134ac
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-12 18:43:43 +0300

    add 012. Weather Observation Station 15

---
+++ SQL/03. Aggregation/012. Weather Observation Station 15.sql
@@ -0,0 +1,11 @@
+-- This is the solution to https://www.hackerrank.com/challenges/weather-observation-station-15/problem
+
+
+SELECT ROUND(Long_W, 4)
+FROM (
+    SELECT Long_W
+    FROM Station
+    WHERE Lat_N < 137.2345
+    ORDER BY Lat_N DESC
+    )
+WHERE ROWNUM = 1;
