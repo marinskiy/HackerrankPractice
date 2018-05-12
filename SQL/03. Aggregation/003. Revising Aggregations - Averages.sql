eda5a7df3eacb14dbc56a0530673ae51d28f29eb
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-12 15:41:00 +0300

    add 003. Revising Aggregations - Averages

---
+++ SQL/03. Aggregation/003. Revising Aggregations - Averages.sql
@@ -0,0 +1,6 @@
+-- This is the solution to https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+
+SELECT AVG(Population)
+FROM City
+WHERE District = 'California';
