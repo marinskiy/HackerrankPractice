6bf3440a92f30bf2f1d02945bad1b0ca46cd321e
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-13 15:59:04 +0300

    Total number of problems added

--- Automated readme generation.py
+++ Automated readme generation.py
@@ -21,10 +21,22 @@ def getProblemURL(path):
     return url
 
 
+def getTotalNumberOfProblems():
+    totalNumber = 0
+    folders = getFoldersNames(os.getcwd())
+    for folder in folders:
+        subfolders = getFoldersNames(os.path.join(os.getcwd(), folder))
+        for subfolder in subfolders:
+            totalNumber += len(getFilesNames(os.path.join(os.getcwd(), folder, subfolder)))
+    return totalNumber
+
+
 readmeFile = open('README.md', 'w', encoding='utf-8')
 print('<p align="center"><a href="https://www.hackerrank.com/marinskiy"><img src="https://i0.wp.com/gradsingames.com/wp-content/uploads/2016/05/856771_668224053197841_1943699009_o.png" ></a></p>', file=readmeFile)
 print(file=readmeFile)
 print('# Solutions to Hackerrank practice problems', file=readmeFile)
+print('This is a repository contains ' + str(getTotalNumberOfProblems()) + ' solutions to Hackerrank practice problems with Python and Oracle SQL.', file=readmeFile)
+print('Updated daily :) If you liked it please press a star.', file=readmeFile)
 print(file=readmeFile)
 print('[![GitHub last commit](https://img.shields.io/github/last-commit/marinskiy/HackerrankPractice.svg)](https://github.com/marinskiy/HackerrankPractice) ', file=readmeFile)
 print('[![GitHub commit activity the past week, 4 weeks, year](https://img.shields.io/github/commit-activity/y/marinskiy/HackerrankPractice.svg)](https://github.com/marinskiy/HackerrankPractice)', file=readmeFile)
@@ -47,6 +59,5 @@ for folder in folders:
                   + '(https://github.com/marinskiy/HackerrankPractice/blob/master/'
                   + folder.replace(' ', '%20') + '/' + subfolder.replace(' ', '%20') + '/'
                   + file.replace(' ', '%20') + ')', file=readmeFile)
-
 
 readmeFile.close()
