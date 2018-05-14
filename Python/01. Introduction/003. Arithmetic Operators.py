c16e8b12a50ad9072cce97662395913ea2268ebb
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-14 19:23:07 +0300

    add 003. Arithmetic Operators

---
+++ Python/01. Introduction/003. Arithmetic Operators.py
@@ -0,0 +1,6 @@
+# Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/submissions/code/70402456
+# Score: 10
+
+
+a, b = int(input()), int(input())
+print((a + b), (a - b), (a * b), sep='\n')
