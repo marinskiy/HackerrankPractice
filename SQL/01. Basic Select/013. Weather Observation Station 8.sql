24c7b2debb05445d94d6a0a3c4fa42fc6bf6f07b
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-15 20:52:33 +0300

    use REGEXP_LIKE

--- SQL/01. Basic Select/013. Weather Observation Station 8.sql
+++ SQL/01. Basic Select/013. Weather Observation Station 8.sql
@@ -4,6 +4,4 @@
 
 SELECT DISTINCT CITY
 FROM STATION
-WHERE
-    SUBSTR (CITY, LENGTH(CITY), 1) IN ('a', 'e', 'i', 'o', 'u')
-    AND SUBSTR (CITY, 1, 1) IN ('A', 'E', 'I', 'O', 'U');
+WHERE REGEXP_LIKE(City, '^[AEIOU].*[aeiou]$');
