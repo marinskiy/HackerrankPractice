89b4df2fe1c8a533618d13bb51bf346052261550
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-17 17:53:26 +0300

    add 003. What's Your Name

---
+++ Python/03. Strings/003. What's Your Name.py
@@ -0,0 +1,6 @@
+# Problem: https://www.hackerrank.com/challenges/whats-your-name/problem
+# Score: 10
+
+
+def print_full_name(first_name, last_name):
+    print("Hello {} {}! You just delved into python.".format(first_name, last_name))
