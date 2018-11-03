d1fa5f764d87f94f7887561d6fc5bca63c286c95
marinskiy <marinskiy@gmail.com>
2018-11-03 12:35:28 +0300

    add 004. Collections OrderedDict()

---
+++ Python/07. Collections/004. Collections OrderedDict().py
@@ -0,0 +1,13 @@
+# Problem: https://www.hackerrank.com/challenges/py-collections-ordereddict/problem
+# Score: 20
+
+
+from collections import OrderedDict
+
+
+ordered_dictionary = OrderedDict()
+for _ in range(int(input())):
+    item, price = input().rsplit(' ', 1)
+    ordered_dictionary[item] = ordered_dictionary.get(item, 0) + int(price)
+for item in ordered_dictionary:
+    print(item, ordered_dictionary[item])
