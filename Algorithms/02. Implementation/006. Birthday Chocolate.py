5a32f3a75b52338fa7467b263d85eb67ccf22871
marinskiy <marinskiy@gmail.com>
2018-11-04 09:27:38 +0300

    PEP8 formatting

--- Algorithms/02. Implementation/006. Birthday Chocolate.py
+++ Algorithms/02. Implementation/006. Birthday Chocolate.py
@@ -4,4 +4,4 @@
 
 n, s = int(input().strip()), list(map(int, input().split()))
 d, m = map(int, input().split())
-print(sum([sum(s[i : i + m]) == d for i in range(len(s) - m + 1)]))
+print(sum([sum(s[i: i + m]) == d for i in range(len(s) - m + 1)]))
