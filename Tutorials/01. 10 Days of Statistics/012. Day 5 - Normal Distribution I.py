9cbb5324c07f9e1c939a3f5f90e1127fbcdc308c
marinskiy <marinskiy@gmail.com>
2019-03-07 14:58:46 +0300

    add 012. Day 5: Normal Distribution I

---
+++ Tutorials/01. 10 Days of Statistics/012. Day 5 - Normal Distribution I.py
@@ -0,0 +1,18 @@
+# Problem: https://www.hackerrank.com/challenges/s10-normal-distribution-1/problem
+# Score: 30
+
+
+import math
+
+
+def cdf(x, mean, std):
+    return 1/2*(1+math.erf((x-mean) / std / 2**(1/2)))
+
+
+mean = 20
+std = 2
+
+# Less than 19.5 hours
+print(round(cdf(19.5, 20, 2), 3))
+# Between 20 and 22 hours?
+print(round(cdf(22, 20, 2) - cdf(20, 20, 2), 3))
