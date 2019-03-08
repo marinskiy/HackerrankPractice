b6cbfefac75ce17af4f0eafd64c367d7a5ceb476
marinskiy <marinskiy@gmail.com>
2019-03-08 14:26:36 +0300

    update solution

--- Tutorials/01. 10 Days of Statistics/014. Day 6 - The Central Limit Theorem I.py
+++ Tutorials/01. 10 Days of Statistics/014. Day 6 - The Central Limit Theorem I.py
@@ -9,8 +9,15 @@ def cdf(x, mean, std):
     return 1/2*(1+math.erf((x-mean) / std / 2**(1/2)))
 
 
-mean = 49 * 205
-std = 15 * 49**(1/2)
+# inputs
+mean = 205
+std = 15
+n = 49
+target = 9800
+
+# characteristics of sum
+mean_s = n * mean
+std_s = std * n**(1/2)
 
 # Find the probability that sum <= 9800
-print(round(cdf(9800, mean, std), 4))
+print(round(cdf(target, mean_s, std_s), 4))
--- Tutorials/01. 10 Days of Statistics/015. Day 6 - The Central Limit Theorem II.py
+++ Tutorials/01. 10 Days of Statistics/015. Day 6 - The Central Limit Theorem II.py
@@ -8,9 +8,15 @@ import math
 def cdf(x, mean, std):
     return 1/2*(1+math.erf((x-mean) / std / 2**(1/2)))
 
+# inputs
+mean = 2.4
+std = 2
+n = 100
+target = 250
 
-mean = 100 * 2.4
-std = 2 * 100**(1/2)
+# characteristics of sum
+mean_s = n * mean
+std_s = std * n**(1/2)
 
 # Find the probability that sum <= 250
-print(round(cdf(250, mean, std), 4))
+print(round(cdf(target, mean_s, std_s), 4))
