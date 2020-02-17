1609a29f363fa3da5349730461cde15531368f4a
hasnainr <shasnainreza@gmail.com>
2020-02-17 16:59:38 +0500

    Create 002. Input and Output.cpp

---
+++ C++/01. Introduction/002. Input and Output.cpp
@@ -0,0 +1,13 @@
+// Problem: https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+// Score: 5
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << A+B+C << endl;
+    return 0;
+}
