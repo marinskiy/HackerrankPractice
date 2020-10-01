3b9772310cf1e363863643a78d7064b5f284cced
Durjay Dhali <47362849+Durjay-Dhali@users.noreply.github.com>
2020-10-01 17:33:27 +0600

    Update 003. Arithmetic Operators.py

--- Python/01. Introduction/003. Arithmetic Operators.py
+++ Python/01. Introduction/003. Arithmetic Operators.py
@@ -1,6 +1,5 @@
-# Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/submissions/code/70402456
-# Score: 10
-
-
-a, b = int(input()), int(input())
-print((a + b), (a - b), (a * b), sep='\n')
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
